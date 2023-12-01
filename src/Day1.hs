ca216c9c304e72ae12076b381246db87dcde744e
Jimmy Hogoboom <jimmy@differential.com>
2023-12-01 11:46:30 -0600

    solution to 1.1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,24 +1,28 @@
 module Day1 (part1) where
+
 import Data.Char (isDigit)
 
 example = "src/input/day1example.txt"
 
-file = example
+input = "src/input/day1.txt"
+
+file = input
 
 readLine :: String -> String
 readLine = go []
   where
     go out line = case line of
-      (c : cs) | isDigit c -> go (updateNumber out c) cs
-               | otherwise -> go out cs
-      [] -> if length out < 2 then [head out, head out] else out
-    updateNumber nums n
-      | null nums = [n]
-      | otherwise = head nums : [n]
+      (c : cs)
+        | isDigit c -> go (updateNumber out c) cs
+        | otherwise -> go out cs
+      [] -> out
+    updateNumber nums newNum = case nums of
+      (n : _) -> [n, newNum]
+      [] -> [newNum, newNum]
 
 part1 :: IO String
 part1 = do
   contents <- readFile file
   let ls = lines contents
-  let result = map readLine ls
+  let result = map readLine (filter (not . null) ls)
   return $ show $ sum $ map read result
