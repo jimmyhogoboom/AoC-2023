1098f93c98dc8d141cb24fd1c6dabd5a286cd671
Jimmy Hogoboom <jimmy@differential.com>
2023-12-01 11:25:40 -0600

    partial solution for day1 part1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,10 +1,27 @@
 module Day1 (part1) where
+import Data.Char (isDigit)
 
 example = "src/input/day1example.txt"
 
 file = example
 
+readLine :: String -> String
+readLine = go ""
+  where
+    go out line = case line of
+      (c : cs) | isDigit c -> go (updateNumber out c) cs
+               | otherwise -> go out cs
+      [] -> out
+    updateNumber num n
+      | null num = [n]
+      | otherwise = head num : [n]
+
 part1 :: IO String
 part1 = do
   contents <- readFile file
-  return contents
+  let ls = lines contents
+  let result = map readLine ls
+  print result
+  -- _ <- map putStrLn result
+  -- putStrLn $ head result
+  return ""
