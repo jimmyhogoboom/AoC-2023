c13d0c8912b219c12d6e2e354ca0930a056784bc
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2023-12-02 01:08:29 -0600

    provide exhaustive patterns for solution 2.1

--- src/Day2.hs
+++ src/Day2.hs
@@ -6,23 +6,14 @@ import Data.List (dropWhileEnd, find)
 example :: String
 example = "src/input/day2example1.txt"
 
-example2 :: String
-example2 = "src/input/day2example2.txt"
-
 input :: String
 input = "src/input/day2.txt"
 
 file :: String
-file = example
+file = input
 
--- Cubes: red, green, blue
---
 -- The Elf would first like to know which games would have been possible
 -- if the bag contained only 12 red cubes, 13 green cubes, and 14 blue cubes?
-
--- for each value, check against matching value in test Game
---
--- Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 
 wordsWhen p s = case dropWhile p s of
   "" -> []
@@ -51,15 +42,18 @@ picksFromValues values =
    in picks''
 
 gameAndValues :: String -> [String]
-gameAndValues line =
-  let [gameId, valuesString] = splitOn ':' line
-      [_, gameId'] = splitOn ' ' gameId
-   in [gameId', valuesString]
+gameAndValues line = case parts of
+    [gameId, valuesString] -> [getId gameId, valuesString]
+    _ -> []
+  where parts = splitOn ':' line
+        getId = last . splitOn ' '
 
 gameAndPicks :: String -> (Integer, [Pick])
 gameAndPicks line =
-  let [gameId, valuesString] = gameAndValues line
-   in (read gameId, picksFromValues valuesString)
+  let parts = gameAndValues line
+   in case parts of
+    [gameId, valuesString] -> (read gameId, picksFromValues valuesString)
+    _ -> (0, [])
 
 testBag :: [(String, Integer)]
 testBag = [("red", 12), ("green", 13), ("blue", 14)]
@@ -92,5 +86,4 @@ part1 = do
   contents <- readFile file
   let ls = lines contents
   let games = map gameAndPicks ls
-  let (gameId, picks) = head games
   return $ show $ sumValidGameIds games
