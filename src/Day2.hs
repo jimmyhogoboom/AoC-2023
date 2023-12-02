5856461d4b2c88e335f5d84512f0f640770ea81a
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2023-12-01 22:58:45 -0600

    prepare for day 2

---
+++ src/Day2.hs
@@ -0,0 +1,23 @@
+module Day2 (part1) where
+
+import Import
+
+example :: String
+example = "src/input/day2example.txt"
+
+example2 :: String
+example2 = "src/input/day2example2.txt"
+
+input :: String
+input = "src/input/day2.txt"
+
+file :: String
+file = input
+
+part1 :: IO String
+part1 = do
+  -- contents <- readFile file
+  -- let ls = lines contents
+  -- return $ head ls
+  return "..."
+
--- src/Run.hs
+++ src/Run.hs
@@ -4,14 +4,26 @@ module Run (run) where
 
 import Import
 import Day1
+import Day2
 
-day1 :: RIO App (String, String)
+-- TODO: make tool to convert list of parts into these pairs
+type DayResult = [(String, String)]
+
+day1 :: RIO App DayResult
 day1 = do
   p1 <- liftIO Day1.part1
   p2 <- liftIO Day1.part2
-  return (p1, p2)
+  return [("1.1", p1), ("1.2", p2)]
+
+day2 :: RIO App DayResult
+day2 = do
+  p1 <- liftIO Day2.part1
+  -- TODO: p2
+  return [("2.1", p1)]
 
 run :: RIO App ()
 run = do
-  (p1, p2) <- day1
-  logInfo $ displayShow [("1.1", p1), ("1.2", p2)]
+  d1 <- day1
+  d2 <- day2
+  -- TODO: Accept command line arguments to choose the day to print
+  logInfo $ displayShow [d1, d2]
