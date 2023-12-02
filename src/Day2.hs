c8cb26694baf1e357b7635dba9e84b5235a31bfc
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2023-12-02 00:52:57 -0600

    solution to 2.1 example

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,7 +1,7 @@
 module Day2 (part1) where
 
 import Data.Char (isSpace)
-import Data.List (dropWhileEnd)
+import Data.List (dropWhileEnd, find)
 
 example :: String
 example = "src/input/day2example1.txt"
@@ -23,9 +23,6 @@ file = example
 -- for each value, check against matching value in test Game
 --
 -- Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-
-testBag :: [(Integer, String)]
-testBag = [(12, "red"), (13, "green"), (14, "blue")]
 
 wordsWhen p s = case dropWhile p s of
   "" -> []
@@ -64,10 +61,36 @@ gameAndPicks line =
   let [gameId, valuesString] = gameAndValues line
    in (read gameId, picksFromValues valuesString)
 
+testBag :: [(String, Integer)]
+testBag = [("red", 12), ("green", 13), ("blue", 14)]
+
+isPartValid :: PickPart -> Bool
+isPartValid (c, t) = case found of
+  Just (_, limit) -> c <= limit
+  Nothing -> False
+  where
+    found = find ((== t) . fst) testBag
+
+isPickValid :: Pick -> Bool
+isPickValid = all isPartValid
+
+isGameValid :: (Integer, [Pick]) -> Bool
+isGameValid (_, picks) = all isPickValid picks
+
+-- TODO: This pattern keeps coming up. There must be a better way
+sumValidGameIds :: [(Integer, [Pick])] -> Integer
+sumValidGameIds = go 0
+  where
+    go total gs = case gs of
+      (g : rest)
+        | isGameValid g -> go (total + fst g) rest
+        | otherwise -> go total rest
+      [] -> total
+
 part1 :: IO String
 part1 = do
   contents <- readFile file
   let ls = lines contents
   let games = map gameAndPicks ls
   let (gameId, picks) = head games
-  return $ show (gameId, picks)
+  return $ show $ sumValidGameIds games
