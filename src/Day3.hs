c7aa174b614ea89df1960e6fd11e8bed2de03c6d
Jimmy Hogoboom <jimmyhogoboom@gmail.com>
2023-12-03 14:26:25 -0600

    initial work towards parser
    The starting attempt at building a parser in the style of tsoding's
    JSON parser: https://github.com/tsoding/haskell-json/blob/bafd97d96b792edd3e170525a7944b9f01de7e34/Main.hs

--- src/Day3.hs
+++ src/Day3.hs
@@ -4,7 +4,8 @@ module Day3
   )
 where
 
-import Data.Char (isSpace)
+import Control.Applicative (Alternative (empty))
+import Data.Char (isDigit, isSpace, isSymbol)
 import Data.List (dropWhileEnd, find, nub)
 import Import
 
@@ -14,8 +15,8 @@ example = "src/input/day3example1.txt"
 exampleSolution :: String
 exampleSolution = "4361"
 
-input :: String
-input = "src/input/day3.txt"
+puzzleInput :: String
+puzzleInput = "src/input/day3.txt"
 
 file :: String
 file = example
@@ -88,13 +89,98 @@ parseStep = undefined
 
 ---------- End Pt. 1 Initial Attempt -------------
 
+newtype Parser a = Parser
+  { runParser :: String -> Maybe (String, a)
+  }
+
+instance Functor Parser where
+  fmap f (Parser p) =
+    Parser $ \input -> do
+      (input', x) <- p input
+      Just (input', f x)
+
+instance Applicative Parser where
+  pure x = Parser $ \input -> Just (input, x)
+  (Parser p1) <*> (Parser p2) =
+    Parser $ \input -> do
+      (input', f) <- p1 input
+      (input'', a) <- p2 input'
+      Just (input'', f a)
+
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  (Parser p1) <|> (Parser p2) =
+    Parser $ \input -> p1 input <|> p2 input
+
+charP :: Char -> Parser Char
+charP x = Parser f
+  where
+    f (y : ys)
+      | y == x = Just (ys, x)
+      | otherwise = Nothing
+    f [] = Nothing
+
+stringP :: String -> Parser String
+stringP = traverse charP
+
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) =
+  Parser $ \input -> do
+    (input', xs) <- p input
+    if null xs
+      then Nothing
+      else Just (input', xs)
+
+spanP :: (Char -> Bool) -> Parser String
+spanP f =
+  Parser $ \input ->
+    let (token, rest) = span f input
+     in Just (rest, token)
+
+data EngineValue
+  = EngineNumber Integer
+  | EngineSymbol String
+  | EngineNull
+  deriving (Show, Eq)
+
+numberP :: Parser EngineValue
+numberP = EngineNumber . read <$> notNull (spanP isDigit)
+
+symbolP :: Parser EngineValue
+symbolP = EngineSymbol <$> notNull (spanP isEngineSymbol)
+
+separatorP :: Parser String
+separatorP = undefined
+
+isEngineSymbol :: Char -> Bool
+isEngineSymbol c = c `elem` ['*', '#', '+', '$']
+
+engineValue :: Parser EngineValue
+engineValue = numberP <|> symbolP
+
+-- | Creates a parser for a string of type "element1 sep1 element2 sep2 element3"
+-- from a parser for separators (sep1, sep2) and and a parser form elements (element1, element2, element3).
+sepBy :: Parser a   -- Parser for the separators
+      -> Parser b   -- Parser for elements
+      -> Parser [b]
+sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
+
+parseFile fileName parser = do
+  input <- readFile fileName
+  case runParser parser input of
+    Just (x, y)       -> return (x, y)
+    Nothing -> return ("", EngineNull)
+
 part1 :: IO String
 part1 = do
-  contents <- readFile file
-  let ls = lines contents
-      -- parsedLines = map (runReader parseLine) ls
-      answer = "0"
-  return $ show (answer == exampleSolution, answer)
+  -- contents <- readFile file
+  result <- parseFile file engineValue
+  -- let ls = lines contents
+  --     line = head ls
+  --     parsedLine = runParser engineValue line
+      -- answer = "0"
+  -- return $ show (answer == exampleSolution, answer)
+  return $ show result
 
 part2 :: IO String
 part2 = do
